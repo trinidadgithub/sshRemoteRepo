d4333c9300ce7865eaa0a85dfdb857af3d599448
Trinidad Marroquin <trinidad.marroquin@gmail.com>
2023-08-23 18:16:47 -0500

    initial check in

---
+++ src/lib/button.js
@@ -0,0 +1,11 @@
+const Button = (props) => {
+    const btnStyle = {
+        color: #fff,
+        backgroundColor: #0069d9,
+        fontSize: 14px
+    }
+    return (
+        <button style={btnStyle}>{props}</button>
+    )
+}
+export default Button;
