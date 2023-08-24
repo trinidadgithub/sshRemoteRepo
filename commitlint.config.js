5da0e5bfc36651fcbc4cd3dbebde88dd171f52db
Trinidad Marroquin <trinidad.marroquin@gmail.com>
2023-08-24 17:42:55 -0500

    fix(commitlint.config.js): add module extention to increase line buffer

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ['@commitlint/config-conventional']
 }
+
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'body-max-line-length': [0, 'always'],
+      'footer-max-line-length': [0, 'always']
+    }
+}
